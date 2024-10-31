fe59aca4837ecb01ef96b8431a07f9d0f9a0c6fc
Artemio Hdz Cst <artemiohernandecastillo@gmail.com>
2024-10-31 13:44:22 -0600

    Remplace views

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "core/home.html")
+def about(request):
+    return render(request, "portfolio/about.html")
+def portfolio(request):
+    return render(request, "portfolio/portfolio.html")
 def contact(request):
     return render(request, "core/contact.html")
